ea939ab1dc4a57f8a75ba3c4bfbe50014dc5bab1
Martijn Hiemstra <info@greenhighway.nl>
2023-08-30 13:59:10 +0200

    Added erived queries

--- src/main/java/nl/applicatie/recept/persist/CategorieRepository.java
+++ src/main/java/nl/applicatie/recept/persist/CategorieRepository.java
@@ -1,9 +1,21 @@
 package nl.applicatie.recept.persist;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import nl.applicatie.recept.model.Categorie;
 
 public interface CategorieRepository extends CrudRepository<Categorie, Long>{
 
+	// Zoeken op naam (Matched de hele naam)
+	List<Categorie> findByNaam(String naam);
+	
+	// Zoekt die op gedeelte van de naam
+	List<Categorie> findByNaamContaining(String naam);
+	
+	int countByNaam(String naam);
+	
+	int countByNaamContaining(String naam);
+	
 }
--- src/main/java/nl/applicatie/recept/persist/IngredientRepository.java
+++ src/main/java/nl/applicatie/recept/persist/IngredientRepository.java
@@ -1,9 +1,17 @@
 package nl.applicatie.recept.persist;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import nl.applicatie.recept.model.Ingredient;
 
 public interface IngredientRepository extends CrudRepository<Ingredient, Long>{
+	
+	List<Ingredient> findByHalalIsTrue();
+	
+	List<Ingredient> findByHalalIsFalse();
+	
+	List<Ingredient> findByHalal(boolean halal);
 	
 }
