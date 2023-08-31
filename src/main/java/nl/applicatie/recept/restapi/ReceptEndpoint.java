7f5d29d62e18be480873553808df8daf2b2294e2
Janine Jackson <jljackson193@gmail.com>
2023-08-31 09:14:33 +0200

    verwijder functie

--- src/main/java/nl/applicatie/recept/restapi/ReceptEndpoint.java
+++ src/main/java/nl/applicatie/recept/restapi/ReceptEndpoint.java
@@ -1,7 +1,9 @@
 package nl.applicatie.recept.restapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,11 @@ public class ReceptEndpoint {
 	@PostMapping("voegrecepttoe")
 	public void voegReceptToe(@RequestBody Recept recept) {
 		rs.slaReceptOp(recept);
+	}
+	
+	@DeleteMapping("verwijderrecept/{receptid}") 
+	public void verwijderRecept(@PathVariable("receptid")int receptid) {
+		rs.verwijderRecept(receptid);
 	}
 	
 }
